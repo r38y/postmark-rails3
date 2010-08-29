e672adf79f1823d571c7f1726ba59edd87497fe2
Randy Schmidt <randy@forge38.com>
2010-08-29 13:57:20 -0400

    The rest of the tests pass

--- spec/postmark-rails_spec.rb
+++ spec/postmark-rails_spec.rb
@@ -9,20 +9,19 @@ describe "PostmarkRails3" do
     TestMailer.simple_message.deliver
   end
 
-  # it "should allow tagging of message" do
-  #   Postmark.should_receive(:send_through_postmark) do |message|
-  #     message.tag.to_s.should == "delivery"
-  #   end
-  #   TestMailer.tagged_message.deliver
-  # end
-  # 
-  # it "should work with multipart messages" do
-  #   Postmark.should_receive(:send_through_postmark) do |message|
-  #       message.should be_multipart
-  #       message.body_text.should == "hello"
-  #       message.body_html.should == "<b>hello</b>"
-  #   end
-  #   TestMailer.multipart_message.deliver
-  # end
-
+  it "should allow tagging of message" do
+    Postmark.should_receive(:send_through_postmark) do |message|
+      message.tag.to_s.should == "delivery"
+    end
+    TestMailer.tagged_message.deliver
+  end
+  
+  it "should work with multipart messages" do
+    Postmark.should_receive(:send_through_postmark) do |message|
+        message.should be_multipart
+        message.body_text.strip.should == "hello"
+        message.body_html.strip.should == "<b>hello</b>"
+    end
+    TestMailer.multipart_message.deliver
+  end
 end
